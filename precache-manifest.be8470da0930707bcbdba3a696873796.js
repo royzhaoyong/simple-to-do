6035bbcf5a7ff08979d662f05deb5c3bb9f3491c
reactnative <royzhaoyong@hotmail.com>
2020-06-22 18:04:10 -0700

    Updates

---
+++ precache-manifest.be8470da0930707bcbdba3a696873796.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85582fd9949e0eb91c12de0323a8d991",
+    "url": "/royzhaoyong/simple-to-do/index.html"
+  },
+  {
+    "revision": "fad8dbe15c0269fe8267",
+    "url": "/royzhaoyong/simple-to-do/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/royzhaoyong/simple-to-do/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/royzhaoyong/simple-to-do/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad8dbe15c0269fe8267",
+    "url": "/royzhaoyong/simple-to-do/static/js/main.631fe96c.chunk.js"
+  },
+  {
+    "revision": "6c0445de15401745fde0",
+    "url": "/royzhaoyong/simple-to-do/static/js/runtime-main.43f05aff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/royzhaoyong/simple-to-do/static/media/logo.5d5d9eef.svg"
+  }
+]);
